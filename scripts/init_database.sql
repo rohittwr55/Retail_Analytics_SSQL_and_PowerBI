fc4efd0fc4a157ecff1efd768133adcff47b484f
rohittwr55 <32168434+rohittwr55@users.noreply.github.com>
2026-02-20 13:24:02 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+use master;
+
+--Create Database Datawarehouse
+create Database Retail_Analytics;
+
+
+use [Retail_Analytics]
+
+create schema raw_data; 
+go
+
+create schema processed_data;
+go
+
+create schema analytics_data;
+go
