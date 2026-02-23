1bb54a8a416ecbdda2b6dc241f8bcf5325d7481f
rohittwr55 <32168434+rohittwr55@users.noreply.github.com>
2026-02-23 20:26:24 +0530

    Update createTable.sql

--- scripts/Processed_data/createTable.sql
+++ scripts/Processed_data/createTable.sql
@@ -10,3 +10,23 @@ create table processed_data.calender
 (
 	table_date Date
 );
+--------------------created processed data table for customer
+if OBJECT_ID ('processed_data.customer', 'u') is not null
+	drop table processed_data.customer
+create table processed_data.customer
+(
+	CustomerKey	int,
+	Prefix varchar(200),	
+	FirstName varchar(200),	
+	LastName varchar(200),	
+	BirthDate date,	
+	MaritalStatus varchar(200),	
+	Gender varchar(200),	
+	EmailAddress varchar(200),	
+	AnnualIncome int,	
+	TotalChildren int,
+	EducationLevel varchar(200),
+	Occupation varchar(200),	
+	HomeOwner varchar(200)
+);
+-------------------------------------------
